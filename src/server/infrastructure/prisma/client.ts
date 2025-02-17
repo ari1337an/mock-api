b198b3271f42c44a9c00ed2aac81f5181350fd0b
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2025-02-17 23:12:19 +0600

    fix: fixing globalThis

--- src/server/infrastructure/prisma/client.ts
+++ src/server/infrastructure/prisma/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 // Prevent multiple instances of Prisma Client in development
 declare global {
@@ -6,8 +6,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') {
-  global.prisma = prisma;
+if (process.env.NODE_ENV === "development") {
+  globalThis.prisma = prisma;
 }
